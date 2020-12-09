7c5fd6c42b8203259917ed981ebcc0a9ef4d6edf
Josué Martinez <josuearmandomartinez@gmail.com>
2020-12-09 17:10:34 -0500

    Creación de metodos insertar y actualizar en el rest hacia la BBD

--- src/main/java/com/clearminds/smo/jamg/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/smo/jamg/servicios/RestEstudiantes.java
@@ -1,16 +1,40 @@
 package com.clearminds.smo.jamg.servicios;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.clearminds.jamg.dtos.Estudiante;
+import com.clearminds.jamg.servicios.ServicioEstudiante;
 
 @Path("/estudiantes")
 public class RestEstudiantes {
 	
-	
-
-	@Path("/hola")
-	@GET
-	public String saludar() {
-		return "Hola mundo!!";
+	@Path("/insertar")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public void insertar(Estudiante estudiante) {
+		try {
+			ServicioEstudiante srvEstudiantes = new ServicioEstudiante();
+			srvEstudiantes.insertarEstudiante(estudiante);
+			System.out.println("Insertando: " + estudiante);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+	
+	@Path("/actualizar")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public void actualizar(Estudiante estudiante) {
+		try {
+			ServicioEstudiante srvEstudiantes = new ServicioEstudiante();
+			srvEstudiantes.actualizarEstudiante(estudiante);
+			System.out.println("Insertando: " + estudiante);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 }
